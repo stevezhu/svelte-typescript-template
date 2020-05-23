e1f2d76be8baf24b442d1258121d160e7c9e2aaa
Steve Zhu <st.zhu1@gmail.com>
2020-05-23 15:03:51 -0400

    Add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  trailingComma: 'es5',
+  bracketSpacing: false,
+  svelteSortOrder: 'scripts-markup-styles',
+};
